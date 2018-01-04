c4fe1bc40e2967b7690a8b2244e2660226beecd5
mohamadng <yazdanpanah.mohamad@gmail.com>
2018-01-04 23:00:29 +0330

    end of season 6

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var express= require('express');
 
 var app=express();
 
+app.use(function (req, res, next) {
+    if (req.header['x-forwarded-proto'] === 'https'){
+        res.redirect('http://'+req.hostname + req.url);
+    }
+    else{
+        next();
+    }
+});
+
 app.use(express.static('public'));
 
-
-app.listen(3000,function () {
-    console.log('Express server is run on port 3000');
+const PORT = process.env.PORT || 3000 ;
+app.listen(PORT,function () {
+    console.log('Express server is run on port '+PORT);
 });
